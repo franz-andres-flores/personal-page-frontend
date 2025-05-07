2c7d3f5cdfbf6b8ff011f46190c70a7dad64ec23
Franz Flores <andresfloresgallardo@gmail.com>
2025-05-07 13:56:52 -0500

    Instalar quasar

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,12 @@
-import { createApp } from "vue";
+import { createApp } from 'vue';
+import { Quasar } from 'quasar';
+
 import App from "./App.vue";
 import router from "./router";
+import "quasar/dist/quasar.sass";
+import quasarUserOptions from './quasar-user-options';
 
-createApp(App).use(router).mount("#app");
+createApp(App)
+    .use(Quasar, quasarUserOptions)
+    .use(router)
+    .mount("#app");
---
+++ src/quasar-user-options.ts
@@ -0,0 +1,16 @@
+import { Dialog, Loading, Meta, Notify } from 'quasar';
+import lang from 'quasar/lang/es';
+
+import './ui/styles/quasar.scss';
+import '@quasar/extras/material-icons/material-icons.css';
+
+export default {
+  config: {},
+  plugins: {
+    Dialog,
+    Loading,
+    Meta,
+    Notify
+  },
+  lang: lang
+}
